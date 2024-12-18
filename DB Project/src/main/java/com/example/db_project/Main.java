029695cc93ddd4058ec710638d419789d8dabf43
Eng-mostafa-osama <mostafaosama3060@gmail.com>
2024-12-18 19:02:21 +0200

    login prototype

--- DB Project/src/main/java/com/example/db_project/Main.java
+++ DB Project/src/main/java/com/example/db_project/Main.java
@@ -3,16 +3,28 @@ package com.example.db_project;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Image appIcon = new Image("file:resources/ahly logo.png");
-        stage.getIcons().add(appIcon);
+
+        // for the background image
+        /*
+        FileInputStream backgroundImg = new FileInputStream("file:resources/ahly logo.png");
+        Image  Img = new Image(backgroundImg);
+        BackgroundImage backgroundimage = new BackgroundImage(image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        Background background = new Background(backgroundimage);  */
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("homeScreen.fxml"));
         Scene homePage = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("AL Ahly jr's ");
