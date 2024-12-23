d92497176bc487ab2331a9de61fbf83a4cd83f0e
Eng-mostafa-osama <mostafaosama3060@gmail.com>
2024-12-23 16:55:33 +0200

    Update ahly.sql

--- DB Project/DataBase/ahly.sql
+++ DB Project/DataBase/ahly.sql
@@ -1,1 +1,119 @@
 create database Ahly;
+use ahly;
+
+-- Table: User
+CREATE TABLE User (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    UFname VARCHAR(25) NOT NULL,
+    ULname VARCHAR(25) NOT NULL,
+    UAge INT NOT NULL,
+    UPhoneNumber VARCHAR(15),
+    UEmail VARCHAR(40)
+);
+-- Table: Teams
+CREATE TABLE Teams (
+    TeamID INT AUTO_INCREMENT PRIMARY KEY,
+    TName VARCHAR(50) NOT NULL
+);
+
+-- Table: Members
+CREATE TABLE Members (
+    MemberID INT AUTO_INCREMENT PRIMARY KEY,
+    MEmail VARCHAR(40) NOT NULL,
+    MfName VARCHAR(25) NOT NULL,
+    MlName VARCHAR(25),
+    MSubscriptions VARCHAR(20) default 'Not Subscriped',
+    MPhoneNumber VARCHAR(15),
+    TeamID INT,
+    FOREIGN KEY (TeamID) REFERENCES Teams(TeamID)
+);
+
+-- Table: Owner
+CREATE TABLE Owner (
+    OwnerID INT AUTO_INCREMENT PRIMARY KEY,
+    OName VARCHAR(25) NOT NULL,
+    OPhone VARCHAR(15)
+);
+
+-- Table: Subscriptions
+CREATE TABLE Subscriptions (
+    SubscriptionID INT AUTO_INCREMENT PRIMARY KEY,
+    SStartDate DATE NOT NULL,
+    SEndDate DATE NOT NULL,
+    SPlanType VARCHAR(50) default 'Monthly',
+    SAmount DECIMAL(10, 2),
+    UserID INT,
+    MemberID INT,
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID)
+);
+
+-- Table: Expenses
+CREATE TABLE Expenses (
+    ExpenseID INT AUTO_INCREMENT PRIMARY KEY,
+    EExpenseType VARCHAR(50),
+    EAmount DECIMAL(10, 2),
+    OwnerID INT,
+    TeamID INT,
+    FOREIGN KEY (OwnerID) REFERENCES Owner(OwnerID),
+    FOREIGN KEY (TeamID) REFERENCES Teams(TeamID)
+);
+
+alter table teams
+add FOREIGN KEY (ExpenseID) REFERENCES Expenses(ExpenseID) ;
+
+-- Table: Team Members
+CREATE TABLE TeamMembers (
+    Role VARCHAR(50) ,
+    TeamID INT,
+    MemberID INT,
+    PRIMARY KEY (TeamID, MemberID),
+    FOREIGN KEY (TeamID) REFERENCES Teams(TeamID),
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID)
+);
+
+-- Table: Training Sessions
+CREATE TABLE TrainingSessions (
+    SessionID INT AUTO_INCREMENT PRIMARY KEY,
+    TRDuration int NOT NULL,
+    TRDate DATE NOT NULL,
+    TeamID INT,
+    FOREIGN KEY (TeamID) REFERENCES Teams(TeamID)
+);
+
+-- Table: Coaches
+CREATE TABLE Coaches (
+    CoachID INT AUTO_INCREMENT PRIMARY KEY,
+    CEmail VARCHAR(50),
+    CfName VARCHAR(40),
+    ClName VARCHAR(40),
+    CPhone VARCHAR(15),
+    CAge INT,
+    TeamID INT,
+    FOREIGN KEY (TeamID) REFERENCES Teams(TeamID)
+);
+
+-- Table: Manages
+CREATE TABLE Manages (
+    OwnerID INT,
+    TeamID INT,
+    PRIMARY KEY (OwnerID, TeamID),
+    FOREIGN KEY (OwnerID) REFERENCES Owner(OwnerID),
+    FOREIGN KEY (TeamID) REFERENCES Teams(TeamID)
+);
+
+alter table user
+add constraint chk_Uage
+check (Uage>0);
+
+alter table Coaches
+add constraint chk_Cage
+check (Cage>0);
+
+alter table Subscriptions
+add constraint chk_Samount
+check (Samount>0);
+
+alter table expenses
+add constraint chk_Eamount
+check (Eamount>0);
